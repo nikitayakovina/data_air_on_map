470c91ded702d80608615db35bc78d842c9b96ff
nikitayakovina <yakovinans@mir-omsk.ru>
2022-04-30 12:53:58 +0600

    ~ обновил меню, починил карты

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {LocationService} from "../service/location-service.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   constructor() {}
+  public ngOnInit(): void {}
 }
