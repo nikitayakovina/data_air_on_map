24f5443ac3d87eeecc4f0b6e76c29a93cc031093
ya-fi-nikita@yandex.ru <ya-fi-nikita@yandex.ru>
2022-05-03 22:52:18 +0600

    ~ services folders

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,7 +14,7 @@ import {MatButtonModule} from "@angular/material/button";
 import {TableModule} from "primeng/table";
 import {MatProgressBarModule} from "@angular/material/progress-bar";
 import { YandexMapsComponent } from './yandex-maps/yandex-maps.component';
-import { HttpService } from './service/http-service.service';
+import { HttpService } from '../service/http-service.service';
 const mapConfig: YaConfig = {
   apikey: '2cd6704d-9e98-4e03-b754-681f18196dca',
   lang: 'ru_RU',
